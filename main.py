ec4e1f926985682a19f9f42830447c2a806ce5e1
Lee-sun-hang <dltjsgod0823@gmail.com>
2022-09-09 22:40:43 +0900

    swagger 및 flask 연동
    swagger 및 flask 연동

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restx import Api, Resource, reqparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
+
+test_api = api.namespace('test', description='조회 API')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@test_api.route('/')
+class Test(Resource):
+    def get(self):
+    	return 'Hello World!'
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('commit test')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
